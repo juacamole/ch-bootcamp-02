a0229880faf969868f95b9fceb5663d420ae8d75
LUEJ9 <juan.luethy@coop.ch>
2024-02-27 09:03:31 +0100

    Java Script of Day 2 Task 1

---
+++ js/2.1.js
@@ -0,0 +1,44 @@
+let age = 15;
+
+for (let i = 0; i <= age; i++){
+  console.log(i)
+}
+if (age >= 18){
+  console.log("yes");
+} else {
+  console.log("no")
+}
+
+let score = 0;
+
+if (score !== 0){
+  console.log("Score is available");
+}
+
+if (score){
+  console.log("Score is evaluated as truthy.")
+} else {
+  console.log("Score is not evaluated as truthy.")
+}
+
+let username = null;
+
+if (username != null){
+  console.log("Username is available");
+}
+
+if(!username){
+  console.log("Username is evaluated as falsy.")
+} else {
+  console.log("Username is not evaluated as falsy.")
+}
+
+let isAdmin = false;
+
+if (isAdmin){
+  console.log("isAdmin is evaluated as truthy.");
+} else if (!isAdmin){
+  console.log("isAdmin is false.")
+}
+
+
